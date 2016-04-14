a926baf9538b981865218848bfb456e81c2993d8
Jamie <jamie@blueshoon.com>
2016-04-14 17:06:00 -0500

    initial group of files for static HTML

---
+++ static/lib/global-navigation.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-dark navbar-main bg-transparent">
+  <a class="navbar-brand hidden-print" href="/static/overview.php"><span class="brand-logo hide-text">Timeframe</span></a>
+
+  <ul class="nav navbar-nav pull-sm-right">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Overview</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">User Preferences</a>
+    </li>
+    <li class="nav-item">
+      <a class="btn btn-default btn-sm" href="#">Log out</a>
+    </li>
+  </ul>
+</nav>
---
+++ static/overview.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Timeframe | Overview</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link type="text/css" rel="stylesheet" href="assets/css/style.css" media="all">
+<script type='text/javascript' src='http://fast.fonts.net/jsapi/52d091f9-f8ff-4b93-9cd6-aeca0d7761f4.js?ver=4.4.2'></script>
+</head>
+
+<body class="page-global" style="background-image: url(assets/images/bg-admin-overview.jpg);">
+	<div class="container container-head">
+		<?php include("lib/global-navigation.php"); ?>
+	</div>
+	<div class="container content-container">
+		<div class="content-area row">
+			<section id="overview" class="page-section">
+				<div class="section-content">
+					<div class="row">
+						<div class="col-md-2">
+							<?php include("lib/brand-link.php"); ?>
+						</div>
+						<div class="col-md-6">
+							<?php include("lib/reminder-list.php"); ?>
+						</div>
+						<div class="col-md-4">
+							<?php include("lib/summary-list.php"); ?>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</body>
+</html>
