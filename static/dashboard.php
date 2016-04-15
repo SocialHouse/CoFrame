c72c74cff01e6401d08e9ef3fb85b400203ce648
Jamie <jamie@blueshoon.com>
2016-04-15 15:12:16 -0500

    brand dashboard updates

---
+++ static/dashboard.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Timeframe | Overview</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link type="text/css" rel="stylesheet" href="assets/css/style.css" media="all">
+<script type='text/javascript' src='http://fast.fonts.net/jsapi/52d091f9-f8ff-4b93-9cd6-aeca0d7761f4.js'></script>
+<script type='text/javascript' src='assets/js/vendor/modernizr.3.3.1.custom.js?ver=3.3.1'></script>
+</head>
+
+<body class="page-global" style="background-image: url(assets/images/bg-brand-management.jpg);">
+	<div class="container container-head">
+		<?php include("lib/global-navigation.php"); ?>
+	</div>
+	<div class="container content-container">
+		<div class="content-area row">
+			<section class="brand-navigation bg-white opaque-88 col-sm-2">
+				<?php include("lib/brand-navigation.php"); ?>
+				<div class="current-user-details">
+					<div class="user-name">Master</div>
+					<div class="user-time row">
+						<div class="col-md-5">Current Brand Time </div>
+						<div class="col-md-7 text-md-right current-time">
+							<strong><?php echo date('g:i'); ?></strong><br>
+							<?php echo date('a T'); ?>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="brand-manage" class="page-main bg-white col-sm-10">
+				<header class="page-main-header">
+					<h1 class="center-title section-title">Brand Dashboard</h1>
+				</header>
+				<div class="col-sm-12 date-header"><strong><?php echo date('F'); ?></strong> <?php echo date('d') . ", " . date('Y'); ?></div>
+				<div class="row">
+					<div class="col-md-6">
+						<?php include("lib/reminder-list.php"); ?>
+					</div>
+					<div class="col-md-3">
+						<?php include("lib/summary-list.php"); ?>
+					</div>
+					<div class="col-md-3">
+						<?php include("lib/calendar-today-summary.php"); ?>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+
+	<script type='text/javascript' src='assets/js/vendor/jquery.js?ver=1.11.3'></script>
+	<script type='text/javascript' src='assets/js/vendor/tether.min.js'></script>
+	<script type='text/javascript' src='assets/js/vendor/bootstrap.min.js?ver=4.0.0'></script>
+	<script type='text/javascript' src='assets/js/main.js?ver=1.0.0'></script>
+</body>
+</html>
---
+++ static/lib/reminder-list.php
@@ -0,0 +1,19 @@
+<h3>Reminders <i class="fa fa-question-circle-o" data-toggle="popover" data-placement="bottom" data-content="Whatever cray disrupt ethical. Williamsburg wolf pabst meh blue bottle next level. Blue bottle flannel locavore pour-over, letterpress gluten-free fap ethical polaroid wayfarers trust fund man braid skateboard."></i></h3>
+
+<ul class="reminder-list timeframe-list">
+	<li>Review feedback from David W. on Facebook post 8/29<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Approve Facebook post 8/31<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Review feedback from Kristin P. on Twitter post 9/2 </li>
+	<li>Review feedback from Jose A. on Vine post 9/3</li>
+	<li>Review Twitter post for 9/5</li>
+	<li>Review feedback from David W. on Facebook post 8/29<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Approve Facebook post 8/31<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Review feedback from Kristin P. on Twitter post 9/2 </li>
+	<li>Review feedback from Jose A. on Vine post 9/3</li>
+	<li>Review Twitter post for 9/5</li>
+	<li>Review feedback from David W. on Facebook post 8/29<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Approve Facebook post 8/31<div class="pull-sm-right"><i class="fa fa-exclamation-circle color-danger"></i></div></li>
+	<li>Review feedback from Kristin P. on Twitter post 9/2 </li>
+	<li>Review feedback from Jose A. on Vine post 9/3</li>
+	<li>Review Twitter post for 9/5</li>
+</ul>
