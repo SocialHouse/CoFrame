b1b53230b45cc9d6b3e467553724471ecc34675e
Jamie <jamie@blueshoon.com>
2016-04-28 17:05:19 -0500

    initial styles and markup for add brand step 4 - tags

--- static/lib/add-brand-step-4.php
+++ static/lib/add-brand-step-4.php
@@ -1,8 +1,48 @@
 <div class="container-brand-step">
 	<h3 class="text-xs-center">Step 4</h3>
-	<h4 class="text-xs-center">Post Tags</h4>
+	<h4 class="text-xs-center">Post Tags<i class="fa fa-question-circle-o" tabindex="0" data-toggle="popover" data-placement="bottom" data-content="Whatever cray disrupt ethical. Williamsburg wolf pabst meh blue bottle next level. Blue bottle flannel locavore pour-over, letterpress gluten-free fap ethical polaroid wayfarers trust fund man braid skateboard." data-popover-arrow="true"></i></h4>
+	<a href="#brandOutlets" id="addTagLink" class="border-top border-bottom border-black add-link show-hide" data-hide="#addTagLink, #outletStepBtns" data-show="#selectTags, #addTagBtns"><i class="tf-icon circle-border">+</i>Add Tag</a>
+	<div id="selectTags" class="hidden">
+		<h5 class="text-xs-center border-bottom border-black ">Add a Tag</h5>
+		<h5 class="border-title"><span>Select Tag Color</span></h5>
+		<div class="tag-list">
+			<ul>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-red"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-pink"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-orange"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-yellow"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-green"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-green-dark"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-blue"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-blue-dark"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-purple-dark"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-purple"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-brown"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-tan"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag"><input type="checkbox" class="hidden-xs-up" name="brand-tag" value=""><i class="fa fa-circle tag-gray"></i></li>
+				<li class="tag" data-value="" data-group="brand-tag" id="colorPicker"><i class="tf-icon circle-border">+</i></li>
+			</ul>
+		</div>
+		<div class="form-group">
+			<select class="form-control">
+				<option value="">Select Label</option>
+				<option value="Marketing">Marketing</option>
+				<option value="E-Commerce">E-Commerce</option>
+				<option value="Sales">Sales</option>
+				<option value="Promotion">Promotion</option>
+				<option value="other">+ Add Label</option>
+			</select>
+		</div>
+	</div>
 	<footer class="post-content-footer">
-		<button class="btn btn-sm btn-default">Cancel</button>
-		<button class="btn btn-sm btn-disabled pull-sm-right btn-next-step" disabled data-active-class="btn-secondary" data-next-step="4">Next</button>
+		<div id="outletStepBtns">
+			<div class="disclaimer"><button class="btn btn-sm btn-default" type="submit">Skip this Step</button></div>
+			<button type="button" class="btn btn-sm btn-default btn-next-step" data-next-step="3">Back</button>
+			<button class="btn btn-sm btn-disabled pull-sm-right btn-next-step btn-secondary" disabled data-next-step="4">Done</button>
+		</div>
+		<div id="addTagBtns" class="hidden">
+			<button type="button" class="btn btn-sm btn-default">Cancel</button>
+			<button class="btn btn-sm btn-disabled pull-sm-right btn-secondary">Add</button>
+		</div>
 	</footer>
 </div>
