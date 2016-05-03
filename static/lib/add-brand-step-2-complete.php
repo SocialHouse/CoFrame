1f1f549f1010565819dce3b1bb39405983a98f44
jamiedoherty <jamie@blueshoon.com>
2016-05-03 13:40:06 -0500

    new files for add brand complete view

---
+++ static/lib/add-brand-step-2-complete.php
@@ -0,0 +1,49 @@
+<div class="container-brand-step">
+	<h3 class="text-xs-center">Step 2</h3>
+	<h4 class="text-xs-center">Brand Outlets</h4>
+	<div class="outlet-list saved-items">
+		<ul>
+			<li data-outlet="youtube"><i class="fa fa-youtube-play"><span class="bg-outlet bg-youtube"></span></i>youtube</li>
+			<li data-outlet="tumblr"><i class="fa fa-tumblr"><span class="bg-outlet bg-tumblr"></span></i>tumblr</li>
+			<li data-outlet="facebook"><i class="fa fa-facebook"><span class="bg-outlet bg-facebook"></span></i>facebook</li>
+			<li data-outlet="vimeo"><i class="fa fa-vimeo"><span class="bg-outlet bg-vimeo"></span></i>vimeo</li>
+			<li data-outlet="pinterest"><i class="fa fa-pinterest"><span class="bg-outlet bg-pinterest"></span></i>pinterest</li>
+			<li data-outlet="wordpress"><i class="fa fa-wordpress"><span class="bg-outlet bg-wordpress"></span></i>wordpress</li>
+		</ul>
+	</div>
+	<div class="add-brand-details brand-fields border-bottom border-black hidden">
+		<div id="selectedOutlets" class="outlet-list selected-items">
+			<ul>
+			</ul>
+		</div>		
+		<a href="#brandOutlets" id="addOutletLink" class="border-top border-bottom border-black add-link show-hide" data-hide="#addOutletLink, #outletStepBtns, #selectedOutlets" data-show="#brandOutlets, #addOutletBtns"><i class="tf-icon circle-border">+</i>Add Outlet</a>
+		<div id="brandOutlets" class="outlet-list">
+			<h5 class="text-xs-center border-bottom border-black ">Add an Outlet</h5>
+			<ul>
+				<li class="disabled" data-selected-outlet="facebook"><i class="fa fa-facebook"><span class="bg-outlet bg-facebook"></span></i></li>
+				<li class="disabled" data-selected-outlet="twitter"><i class="fa fa-twitter"><span class="bg-outlet bg-twitter"></span></i></li>
+				<li class="disabled" data-selected-outlet="instagram"><i class="fa fa-instagram"><span class="bg-outlet bg-instagram"></span></i></li>
+				<li class="disabled" data-selected-outlet="linkedin"><i class="fa fa-linkedin"><span class="bg-outlet bg-linkedin"></span></i></li>
+				<li class="disabled" data-selected-outlet="vine"><i class="fa fa-vine"><span class="bg-outlet bg-vine"></span></i></li>
+				<li class="disabled" data-selected-outlet="pinterest"><i class="fa fa-pinterest"><span class="bg-outlet bg-pinterest"></span></i></li>
+				<li class="disabled" data-selected-outlet="tumblr"><i class="fa fa-tumblr"><span class="bg-outlet bg-tumblr"></span></i></li>
+				<li class="disabled" data-selected-outlet="youtube"><i class="fa fa-youtube-play"><span class="bg-outlet bg-youtube"></span></i></li>
+				<li class="disabled" data-selected-outlet="google"><i class="fa fa-google-plus"><span class="bg-outlet bg-google-plus"></span></i></li>
+				<li class="disabled" data-selected-outlet="vimeo"><i class="fa fa-vimeo"><span class="bg-outlet bg-vimeo"></span></i></li>
+				<li class="disabled" data-selected-outlet="wordpress"><i class="fa fa-wordpress"><span class="bg-outlet bg-wordpress"></span></i></li>
+				<li class="disabled" data-selected-outlet="blogger"><i class="icon-blogger"><span class="bg-outlet bg-blogger"></span></i></li>
+			</ul>
+			<input type="hidden" id="brandOutlet">
+		</div>
+	</div>
+	<footer class="post-content-footer">
+		<div id="outletStepBtns" class="hidden">
+			<button type="button" class="btn btn-sm btn-default btn-next-step" data-next-step="1">Back</button>
+			<button type="button" class="btn btn-sm btn-disabled pull-sm-right btn-next-step" data-next-step="3" data-active-class="btn-secondary">Next</button>
+		</div>
+		<div class="hidden" id="addOutletBtns">
+			<button type="button" class="btn btn-sm btn-default btn-cancel show-hide" data-hide="#addOutletBtns, #brandOutlets" data-show="#outletStepBtns, #addOutletLink, #selectedOutlets">Cancel</button>
+			<button type="button" class="btn btn-sm btn-disabled btn-secondary pull-sm-right show-hide" disabled id="addOutlet" data-hide="#addOutletBtns, #brandOutlets" data-show="#outletStepBtns, #addOutletLink, #selectedOutlets">Add</button>
+		</div>
+	</footer>
+</div>
---
+++ static/lib/add-brand-step-3-complete.php
@@ -0,0 +1,137 @@
+<div class="container-brand-step">
+	<h3 class="text-xs-center">Step 3</h3>
+	<h4 class="text-xs-center">Users &amp; Permissions</h4>
+	<div class="user-permissions-list">
+		<div class="clearfix">
+			<div class="pull-sm-right">
+				<div class="table-header">
+					<div class="permission">Create</div>
+				</div>
+				<div class="table-header">
+					<div class="permission">Edit</div>
+				</div>
+				<div class="table-header">
+					<div class="permission">Approve</div>
+				</div>
+				<div class="table-header">
+					<div class="permission">View Content</div>
+				</div>
+				<div class="table-header">
+					<div class="permission">Brand Settings</div>
+				</div>
+				<div class="table-header">
+					<div class="permission">Billing</div>
+				</div>
+			</div>
+		</div>
+		<div class="table">
+			<div class="table-cell">
+				<div class="pull-sm-left"><img src="assets/images/fpo/norel.jpg" width="36" height="36" alt="Norel Mancuso" class="circle-img"/></div>
+				<div class="pull-sm-left post-approver-name"><strong>Norel Mancuso</strong>Master Admin</div>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+		</div>
+		<div class="table">
+			<div class="table-cell">
+				<div class="pull-sm-left"><img src="assets/images/fpo/david.jpg" width="36" height="36" alt="David Weinberg" class="circle-img"/></div>
+				<div class="pull-sm-left post-approver-name"><strong>David Weinberg</strong>Manager</div>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+		</div>
+		<div class="table">
+			<div class="table-cell">
+				<div class="pull-sm-left"><img src="assets/images/fpo/kristin.jpg" width="36" height="36" alt="David Weinberg" class="circle-img"/></div>
+				<div class="pull-sm-left post-approver-name"><strong>Kristin Patrick</strong>Approver</div>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+		</div>
+		<div class="table border-bottom border-black">
+			<div class="table-cell">
+				<div class="pull-sm-left"><img src="assets/images/fpo/johan.jpg" width="36" height="36" alt="David Weinberg" class="circle-img"/></div>
+				<div class="pull-sm-left post-approver-name"><strong>Johan Loekito</strong>Creator</div>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+				<i class="fa fa-check"></i>
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+			<div class="table-cell text-xs-center vertical-middle has-permission">
+			</div>
+		</div>
+	</div>
+	<div class="hidden brand-fields">
+		<?php include("user-permission-list.php"); ?>
+		<a href="#addUser" id="addUserLink" class="border-top border-bottom border-black add-link show-hide" data-hide="#addUserLink, #outletStepBtns, #userPermissionsList" data-show="#addNewUser, #addUserBtns"><i class="tf-icon circle-border">+</i>Add User</a>
+		<?php include("add-new-user.php"); ?>
+		<?php include("add-user-roles.php"); ?>
+	</div>
+	<footer class="post-content-footer">
+		<div class="hidden" id="outletStepBtns">
+			<button type="button" class="btn btn-sm btn-default btn-next-step" data-next-step="2">Back</button>
+			<button type="button" class="btn btn-sm pull-sm-right btn-next-step btn-secondary"  data-next-step="4">Next</button>
+		</div>
+		<div class="hidden" id="addUserBtns">
+			<button type="button" class="btn btn-sm btn-default btn-cancel show-hide" data-hide="#addNewUser, #addUserBtns" data-show="#addUserLink, #outletStepBtns, #userPermissionsList">Cancel</button>
+			<button type="button" class="btn btn-sm btn-disabled btn-secondary pull-sm-right show-hide" id="addRole" data-hide="#addNewUser, #addUserBtns" data-show="#userRoleBtns, #addUserRole">Role</button>
+		</div>
+		<div class="hidden" id="userRoleBtns">
+			<p class="disclaimer">Upon clicking ‘Add,’ a registration link will be sent to this user.</p>
+			<button type="button" class="btn btn-sm btn-default btn-cancel show-hide" data-hide="#addUserRole, #userRoleBtns" data-show="#addUserLink, #outletStepBtns, #userPermissionsList">Cancel</button>
+			<button type="button" class="btn btn-sm btn-disabled btn-secondary pull-sm-right show-hide" disabled id="addRole" data-hide="#addNewUser, #addUserBtns" data-show="#userRoleBtns, #addUserRole">Add</button>
+		</div>
+	</footer>
+</div>
