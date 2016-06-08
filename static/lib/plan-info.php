299f2e32c1f59d8402e6fb3f7ce04ae9f821459f
Jamie <jamie@blueshoon.com>
2016-06-08 12:26:17 -0500

    user preferences

---
+++ static/lib/plan-info.php
@@ -0,0 +1,62 @@
+<div class="btn-group"> <a href="user-preferences.php" class="btn btn-sm">My Info</a> <a href="user-plan.php" class="btn btn-sm active">Plan</a> <a href="user-billing.php" class="btn btn-sm">Billing</a> </div>
+
+<div class="row table">
+	<div class="col-md-3 text-center col-sm-6 table-cell pricing-details">
+		<header class="price-title">
+			<h2>Start-Up</h2>
+			<h3>$99 <small>per month</small></h3>
+		</header>
+		<ul>
+			<li>Lorem ipsum dolor sit</li>
+			<li>Amet consectetur</li>
+			<li>Adipiscing elit sed do euis</li>
+			<li>Mod tempor incididunt</li>
+		</ul>
+		<a class="btn btn-secondary btn-sm btn-choose-plan" data-plan="Start-Up" data-price="$99.00">Select</a>
+	</div>
+	<div class="col-md-3 text-center col-sm-6 table-cell pricing-details active-plan">
+		<header class="price-title">
+			<h2>Business</h2>
+			<h3>$199 <small>per month</small></h3>
+		</header>
+		<ul>
+			<li>Lorem ipsum dolor sit</li>
+			<li>Amet consectetur</li>
+			<li>Adipiscing elit sed do euis</li>
+			<li>Mod tempor incididunt</li>
+			<li>Ut labore et dolore magn</li>
+		</ul>
+		<a class="btn btn-secondary btn-sm btn-choose-plan btn-disabled" data-plan="Business" data-price="$199.00">Active</a>
+	</div>
+	<div class="col-md-3 text-center col-sm-6 table-cell pricing-details">
+		<header class="price-title">
+			<h2>Corporate</h2>
+			<h3>$299 <small>per month</small></h3>
+		</header>
+		<ul>
+			<li>Lorem ipsum dolor sit</li>
+			<li>Amet consectetur</li>
+			<li>Adipiscing elit sed do euis</li>
+			<li>Mod tempor incididunt</li>
+			<li>ut labore et dolore magn</li>
+			<li>aliqua ut enim ad minim</li>
+		</ul>
+		<a class="btn btn-secondary btn-sm btn-choose-plan" data-plan="Corporate" data-price="$299.00">Select</a>
+	</div>
+	<div class="col-md-3 text-center col-sm-6 table-cell pricing-details">
+		<header class="price-title">
+			<h2>Premiere</h2>
+			<h3>$499 <small>per month</small></h3>
+		</header>
+		<ul>
+			<li>Lorem ipsum dolor sit</li>
+			<li>Amet consectetur</li>
+			<li>Adipiscing elit sed do euis</li>
+			<li>Mod tempor incididunt</li>
+			<li>ut labore et dolore magn</li>
+			<li>aliqua ut enim ad minim</li>
+			<li>veniam quis nostrud</li>
+		</ul>
+		<a class="btn btn-secondary btn-sm btn-choose-plan" data-plan="Premiere" data-price="$499.00">Select</a>
+	</div>
+</div>
---
+++ static/user-plan.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js">
+<head>
+<meta charset="utf-8">
+<title>Timeframe | Overview</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link type="text/css" rel="stylesheet" href="assets/css/style.css" media="all">
+<script type='text/javascript' src='http://fast.fonts.net/jsapi/52d091f9-f8ff-4b93-9cd6-aeca0d7761f4.js'></script>
+<script type='text/javascript' src='assets/js/vendor/modernizr.3.3.1.custom.js?ver=3.3.1'></script>
+</head>
+
+<body class="page-global" style="background-image: url(assets/images/bg-admin-overview.jpg);">
+	<div class="container container-head">
+		<?php include("lib/global-navigation.php"); ?>
+	</div>
+	<div class="container content-container">
+		<div class="content-area row animated fadeIn">
+			<section id="user-preferences" class="page-main bg-white col-sm-12">
+				<header class="page-main-header">
+					<h1 class="center-title section-title">User Preferences</h1>
+				</header>
+				<div class="row">
+					<div class="col-lg-8 col-sm-10 center-block text-xs-center">
+						<?php include("lib/plan-info.php"); ?>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+
+	<script type='text/javascript' src='assets/js/vendor/jquery.js?ver=1.11.3'></script>
+	<script type='text/javascript' src='assets/js/vendor/jquery-ui-sortable.min.js'></script>
+	<script type='text/javascript' src='assets/js/vendor/jquery.qtip.min.js'></script>
+	<script type='text/javascript' src='assets/js/vendor/bootstrap.min.js?ver=4.0.0'></script>
+	<script type='text/javascript' src='assets/js/main.js?ver=1.0.0'></script>
+</body>
+</html>
