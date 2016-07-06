7b17c839f78e26a39750209755a84309557f63ac
Nikhil Kamble <nikhilk@techfivesystems.com>
2016-07-06 16:26:35 +0530

    remove dropdown of language

--- application/views/user_preferences/preference_layout.php
+++ application/views/user_preferences/preference_layout.php
@@ -3,11 +3,6 @@
 	<header class="page-main-header">
 		<h1 class="center-title section-title">User Preferences</h1>
 	</header>
-	<select style="float:right;" onchange="javascript:window.location.href='<?php echo base_url(); ?>welcome/switch_language/'+this.value;">
-	    <option value="english" <?php if($this->session->userdata('site_lang') == 'english') echo 'selected="selected"'; ?>>English</option>
-	    <option value="french" <?php if($this->session->userdata('site_lang') == 'french') echo 'selected="selected"'; ?>>French</option>
-	    <option value="german" <?php if($this->session->userdata('site_lang') == 'german') echo 'selected="selected"'; ?>>German</option>   
-	</select>
 	<div class="row">
 		<?php 
 			if(!empty($view_name)){
