bf2dfa18748a9ac8335481217940abe40ecaf26f
Jamie <jamie@blueshoon.com>
2016-07-08 16:36:55 -0500

    back button

--- application/views/approvals/edit_request.php
+++ application/views/approvals/edit_request.php
@@ -1,6 +1,9 @@
 <?php $this->load->view('partials/brand_nav'); ?>
 <section id="brand-manage" class="page-main bg-white col-sm-10">
 	<header class="page-main-header">
+		<a href="<?php echo base_url().'approvals/'.$brand->slug; ?>" class="btn btn-default btn-sm btn-arrow-left pull-left"><i class="fa fa-angle-left"></i>Back</a>
+		<!-- invisible button hack to maintain centering of title-->
+		<a href="<?php echo base_url().'approvals/'.$brand->slug; ?>" class="btn btn-default btn-sm btn-arrow pull-right invisible"><i class="fa fa-angle-left"></i>Back</a>
 		<h1 class="center-title section-title">Post Approval</h1>
 	</header>
 
