95cd4829f469acfb17167252efade910f954d558
Jamie <jamie@blueshoon.com>
2016-07-21 17:20:22 -0500

    fixes to markup of pinterest post edit view

--- application/views/calendar/post_preview/pinterest.php
+++ application/views/calendar/post_preview/pinterest.php
@@ -11,45 +11,48 @@ if(!empty($post_details)){
 }
 ?>
 <div class="pn_post">
-	<div class="pinterest-post">
-		<div class="post-container">
-			<div class="pinterest-img-div img-div">
-				<?php
-					if(!empty($post_images)){
-						foreach ($post_images as $key) {
-							if (file_exists('uploads/'.$brand_onwer.'/brands/'.$brand_id.'/posts/'.$key->name)) {
-								if($key->type  == 'images' ){
-									echo '<img src="'.base_url().'uploads/'.$brand_onwer.'/brands/'.$brand_id.'/posts/'. $key->name.'"	/>';
-									break;
-								}
+	<div class="post-container">
+		<div class="pinterest-img-div img-div">
+			<?php
+				if(!empty($post_images)){
+					foreach ($post_images as $key) {
+						if (file_exists('uploads/'.$brand_onwer.'/brands/'.$brand_id.'/posts/'.$key->name)) {
+							if($key->type  == 'images' ){
+								echo '<img src="'.base_url().'uploads/'.$brand_onwer.'/brands/'.$brand_id.'/posts/'. $key->name.'"	/>';
+								break;
 							}
 						}
 					}
+				}
+			?>
+		</div>
+		<div class="post-details">
+			<div class="clearfix post-header">
+				<div class="pull-left">
+					<?php 
+						if (file_exists(upload_url().$post_details->created_by.'/users/'.$post_details->user_id.'.png')) {
+							echo '<img src="'.upload_url().$post_details->created_by.'/users/'.$post_details->user_id.'.png" class="user-profile-img" />';
+						}else{
+							echo '<img class="user-profile-img" src="'.img_url().'default_profile.jpg" width="40">';	
+						}
+					?>	
+				</div>
+				<div class="post-user-name pull-left">
+					by <?php echo (!empty($post_details->user))? $post_details->user :''; ?>
+				</div>
+			</div>
+			<div class="post_copy_text">
+				<?php 
+					$content = $post_details->content;
+					$content = replace_with_expression($content);
+					echo (!empty($content)) ? $content : '';
 				?>
 			</div>
-			<div class="post-details">
-				<div class="clearfix post-header">
-					<div class="pull-left">
-						<?php 
-							if (file_exists(upload_url().$post_details->created_by.'/users/'.$post_details->user_id.'.png')) {
-								echo '<img src="'.upload_url().$post_details->created_by.'/users/'.$post_details->user_id.'.png" class="user-profile-img" />';
-							}else{
-								echo '<img class="user-profile-img" src="'.img_url().'default_profile.jpg" width="40">';	
-							}
-						?>	
-					</div>
-					<div class="post-user-name pull-left">
-						by <?php echo (!empty($post_details->user))? $post_details->user :''; ?>
-					</div>
-				</div>
-				<div class="post_copy_text">
-					<?php 
-						$content = $post_details->content;
-						$content = replace_with_expression($content);
-						echo (!empty($content)) ? $content : '';
-					?>
-				</div>
-			</div>
+		</div>
+		<div class="post-credits">
+		<a href="#" target="_blank"><strong>Saved from</strong><br>
+		rothcheese.com
+		</a>
 		</div>
 	</div>
 </div>
