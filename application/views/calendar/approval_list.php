70cb1835c996e6e5b6586e91a278740e5dc896e6
Jamie <jamie@blueshoon.com>
2016-08-03 12:35:36 -0500

    display appropriate approved user data

--- application/views/calendar/approval_list.php
+++ application/views/calendar/approval_list.php
@@ -7,16 +7,25 @@ if(!empty($approved)){
 	<?php 
 		foreach ($approved as $phase) {
 			$name = '';
-			if(!empty($current_phase->first_name)){
-				$name = ucfirst($current_phase->first_name);
-				if(!empty($current_phase->last_name)){
-					$name = $name.' '.ucfirst(substr($current_phase->last_name, 0, 1));
+			if(!empty($phase->first_name)){
+				$name = ucfirst($phase->first_name);
+				if(!empty($phase->last_name)){
+					$name = $name.' '.ucfirst(substr($phase->last_name, 0, 1));
 				}
 			}
 			?>
 			<li class="approved">
-				<div class="pull-sm-left"><img width="36" height="36" class="circle-img" alt="<?php echo $name; ?>" src="http://localhost/timeframe_server/assets/images/default_profile.jpg" /></div>
-				<div class="pull-sm-left">Norel M</div>
+				<div class="pull-sm-left">
+				<?php
+					$image_path = img_url().'default_profile.jpg';
+					if(file_exists(upload_path().$this->user_data['img_folder'].'/users/'.$phase->user_id.'.png'))
+					{
+						$image_path = upload_url().$this->user_data['img_folder'].'/users/'.$phase->user_id.'.png';
+					}
+				?>
+				<img width="36" height="36" class="circle-img" alt="<?php echo $name; ?>" src="<?php echo $image_path; ?>" />
+				</div>
+				<div class="pull-sm-left"><?php echo $name; ?></div>
 			</li>			
 			<?php
 		}
