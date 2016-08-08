0b57579848475cbb26390c25282de33cbaf15a62
Jamie <jamie@blueshoon.com>
2016-08-08 17:08:36 -0500

    update to user preferences nav label

--- application/views/user_preferences/preference_nav.php
+++ application/views/user_preferences/preference_nav.php
@@ -31,7 +31,7 @@
 		?>
 		<a href="<?php echo base_url()?>user_preferences/user_plan" class="btn btn-sm <?php echo $plan; ?>">Plan</a>
 		<a href="<?php echo base_url()?>user_preferences/billing_info" class="btn btn-sm <?php echo $billing; ?>">Billing</a>
-		<a href="<?php echo base_url()?>user_preferences/users" class="btn btn-sm <?php echo $role; ?>">Users</a>
+		<a href="<?php echo base_url()?>user_preferences/users" class="btn btn-sm <?php echo $role; ?>">Account Users</a>
 		<?php
 	// }
 	?>
