830fe811ef66af913a3c4ad19308a4b55e3cc898
Jamie <jamie@blueshoon.com>
2016-08-19 11:26:44 -0500

    initial tweaks for cocreate

--- application/views/co_create/co-create.php
+++ application/views/co_create/co-create.php
@@ -1,9 +1,22 @@
 <?php $this->load->view('partials/brand_nav'); ?>	
 
-<section id="brand-manage" class="page-main bg-white col-sm-10">
-	<header class="page-main-header">
-		<h1 class="center-title section-title">Co-Create</h1>
-	</header>
+<section id="brand-manage" class="page-main bg-white col-sm-10 cocreate-post">
+	<div class="row">
+		<div class="col-sm-8">
+			<header class="page-main-header">
+				<h1 class="center-title section-title">Co-Create</h1>
+			</header>
+		</div>
+		<div class="col-sm-4">
+			<header class="page-main-header">
+				<h1 class="center-title section-title">Discussion</h1>
+			</header>
+			<div class="cocreate-opts text-xs-center">
+				<a href="#"><i class="tf-icon circle-border" data-value="Facebook" data-group="post-outlet"><i class="tf-icon-tele"></i></i></a>
+				<a href="#"><i class="tf-icon circle-border" data-value="Facebook" data-group="post-outlet"><i class="tf-icon-video"></i></i></a>
+			</div>
+		</div>
+	</div>
 	<form action="<?php echo base_url().'posts/save_post' ?>" method="POST" id="post-details" class="file-upload clearfix" upload="<?php echo base_url()."posts/upload"; ?>">
 		<input type="hidden" name="brand_id" id="brand_id" value="<?php echo $brand_id; ?>">
 		<input type="hidden" name="user_id" id="post_user_id" value="<?php echo $this->user_data['account_id']; ?>">
@@ -182,110 +195,78 @@
 			</div>
 
 			<div class="col-md-4 equal-height">
-				<div class="container-approvals">
+				<div class="container-cocreate-discussion">
+					<div class="cocreate-participants"><strong>Participants:</strong> </div>
 					<div class="dafault-phase">
-						<div>
-							<h4 class="text-xs-center">Collaborate</h4>
-								<?php 
-								if(!empty($users))
-								{
-									?>
-									<ul class="timeframe-list user-list first-phase">
-										<?php
-										foreach ($users as $user)
-										{
-											?>
-											<li>
-												<div class="pull-sm-left">
-													<input type="checkbox" data-clear-phase="first" class="hidden-xs-up" name="join_req[]" value="<?php echo $user->aauth_user_id; ?>"><i class="tf-icon check-box circle-border" data-value="<?php echo $user->aauth_user_id; ?>" data-group="join_req[]"><i class="fa fa-check"></i></i>
-												</div>
-												<div class="pull-sm-left">
-													<?php
-													$path = img_url()."default_profile.jpg";
-													if(file_exists(upload_path().$this->user_data['account_id'].'/brands/'.$brand_id.'/posts'.$user->aauth_user_id.'.png'))
-													{
-														$path = upload_url().$this->user_data['account_id'].'/brands/'.$brand_id.'/posts'.$user->aauth_user_id.'.png';
-													}
-													?>
-													<img src="<?php echo $path; ?>" width="36" height="36" alt="<?php echo $user->first_name; ?>" class="circle-img"/>
-												</div>
-												<div class="pull-sm-left post-approver-name">
-													<strong>
-													<?php echo ucfirst($user->first_name)." ".ucfirst($user->last_name); ?>
-													</strong>
-													<?php echo get_user_groups($user->aauth_user_id,$brand_id); ?>
-												</div>
-											</li>										
-											<?php									
-										}
-										?>
-
-										<li class="option-all-users">
-											<div class="pull-sm-left"><i class="tf-icon check-box circle-border" data-value="check-all" data-group="join_req[]"><i class="fa fa-check"></i></i></div>
-											<div class="pull-sm-left"><div class="circle-border bg-black tf-icon">All</div></div>
-											<div class="pull-sm-left post-approver-name">Check<br>All</div>
-										</li>
-									</ul>
-									<?php
-								}
+						<h4 class="text-xs-center">Collaborate</h4>
+							<?php 
+							if(!empty($users))
+							{
 								?>
-								<button id="send-join" class="btn btn-sm btn-secondary submit-approval submit-btn">Send join request</button>
-								<div id="videos">
-							        <div id="subscriber"></div>
-							        <div id="publisher"></div>
-								</div>
+								<ul class="timeframe-list user-list first-phase">
+									<?php
+									foreach ($users as $user)
+									{
+										?>
+										<li>
+											<div class="pull-sm-left">
+												<input type="checkbox" data-clear-phase="first" class="hidden-xs-up" name="join_req[]" value="<?php echo $user->aauth_user_id; ?>"><i class="tf-icon check-box circle-border" data-value="<?php echo $user->aauth_user_id; ?>" data-group="join_req[]"><i class="fa fa-check"></i></i>
+											</div>
+											<div class="pull-sm-left">
+												<?php
+												$path = img_url()."default_profile.jpg";
+												if(file_exists(upload_path().$this->user_data['account_id'].'/brands/'.$brand_id.'/posts'.$user->aauth_user_id.'.png'))
+												{
+													$path = upload_url().$this->user_data['account_id'].'/brands/'.$brand_id.'/posts'.$user->aauth_user_id.'.png';
+												}
+												?>
+												<img src="<?php echo $path; ?>" width="36" height="36" alt="<?php echo $user->first_name; ?>" class="circle-img"/>
+											</div>
+											<div class="pull-sm-left post-approver-name">
+												<strong>
+												<?php echo ucfirst($user->first_name)." ".ucfirst($user->last_name); ?>
+												</strong>
+												<?php echo get_user_groups($user->aauth_user_id,$brand_id); ?>
+											</div>
+										</li>										
+										<?php									
+									}
+									?>
 
-								<br/>
-							
-								<!-- <div id="textchat">
-							        <p id="history"></p>
-							        <form>
-							            <textarea id="msgTxt"></textarea>
-							            <button id="send">Send</button>
-							        </form>
-								</div> -->
+									<li class="option-all-users">
+										<div class="pull-sm-left"><i class="tf-icon check-box circle-border" data-value="check-all" data-group="join_req[]"><i class="fa fa-check"></i></i></div>
+										<div class="pull-sm-left"><div class="circle-border bg-black tf-icon">All</div></div>
+										<div class="pull-sm-left post-approver-name">Check<br>All</div>
+									</li>
+								</ul>
+								<?php
+							}
+							?>
+							<button id="send-join" class="btn btn-sm btn-secondary submit-approval submit-btn">Send join request</button>
+							<div id="videos" class="hidden">
+						        <div id="subscriber"></div>
+						        <div id="publisher"></div>
+							</div>
 
-								<div class="container">
-								    <div class="row chat-window col-xs-5 col-md-3" id="chat_window_1" style="margin-left:10px;">
-								        <div class="col-xs-12 col-md-12">
-								        	<div class="panel panel-default">
-								                <div class="panel-heading top-bar">
-								                    <div class="col-md-8 col-xs-8">
-								                        <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat here</h3>
-								                    </div>
-								                    <div class="col-md-4 col-xs-4" style="text-align: right;">
-								                        <a href="#"><span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim"></span></a>
-								                        <a href="#"><span class="glyphicon glyphicon-remove icon_close" data-id="chat_window_1"></span></a>
-								                    </div>
-								                </div>
-								                <div class="panel-body msg_container_base">
-								                </div>
-								                <div class="panel-footer msg_container_base">
-								                    <div class="input-group">
-								                        <input id="btn-input" type="text" class="form-control input-sm chat_input" placeholder="Write your message here..." />
-								                        <button class="btn btn-primary btn-sm" id="btn-chat">Send</button>
-								                    </div>
-								                </div>
-								    		</div>
-								        </div>
-								    </div>
-								    
-								    <!-- <div class="btn-group dropup">
-								        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-								            <span class="glyphicon glyphicon-cog"></span>
-								            <span class="sr-only">Toggle Dropdown</span>
-								        </button>
-								        <ul class="dropdown-menu" role="menu">
-								            <li><a href="#" id="new_chat"><span class="glyphicon glyphicon-plus"></span> Novo</a></li>
-								            <li><a href="#"><span class="glyphicon glyphicon-list"></span> Ver outras</a></li>
-								            <li><a href="#"><span class="glyphicon glyphicon-remove"></span> Fechar Tudo</a></li>
-								            <li class="divider"></li>
-								            <li><a href="#"><span class="glyphicon glyphicon-eye-close"></span> Invisivel</a></li>
-								        </ul>
-								    </div> -->
-								</div>
+						    <div class="chat-window discussion-list" id="chat_window_1">
+					        	<div class="panel panel-default">
+					                <div class="panel-heading top-bar hidden">
+					                    <div class="col-md-4 col-xs-4" style="text-align: right;">
+					                        <a href="#"><span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim"></span></a>
+					                        <a href="#"><span class="glyphicon glyphicon-remove icon_close" data-id="chat_window_1"></span></a>
+					                    </div>
+					                </div>
+					                <div class="panel-body msg_container_base">
+					                </div>
+					                <div class="panel-footer msg_container_base">
+					                    <div class="input-group">
+					                        <input id="btn-input" type="text" class="form-control chat_input" placeholder="Type a message..." />
+					                        <button class="btn btn-primary btn-sm" id="btn-chat">Send</button>
+					                    </div>
+					                </div>
+					    		</div>
+						    </div>
 
-						</div>
 						<footer class="post-content-footer">
 						<button class="btn btn-sm save-draft-btn submit-btn btn-default"  id="draft">Save Draft</button>
 						<button type="submit" class="btn btn-sm btn-secondary submit-approval submit-btn "  id="submit-approval">Submit for Approval</button>
