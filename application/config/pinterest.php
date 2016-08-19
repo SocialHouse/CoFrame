98108c29c4be43a32241ca00eecfbedce3ab9fee
Nikhil Kamble <nikhilk@techfivesystems.com>
2016-08-19 11:21:29 +0530

    Commented local redirect uri

--- application/config/pinterest.php
+++ application/config/pinterest.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 * Local App Setting
 */
-$config['pinterest_app_id'] = '4822853923106735580';
-$config['pinterest_app_secret'] = 'b7c295f87014b544cc2621dfc63d81ad3383ce6aa07c7239f9fb9c3034733c2b';
-$config['pinterest_callback_url'] = 'https://localhost/timeframe_server/pinterest_connect/pinterest_callback';
+// $config['pinterest_app_id'] = '4822853923106735580';
+// $config['pinterest_app_secret'] = 'b7c295f87014b544cc2621dfc63d81ad3383ce6aa07c7239f9fb9c3034733c2b';
+// $config['pinterest_callback_url'] = 'https://localhost/timeframe_server/pinterest_connect/pinterest_callback';
 
 /*
 * Server App Setting
