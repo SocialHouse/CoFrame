64035d6c6d24eba911de3ca875609aa6ab156b2b
Nikhil Kamble <nikhilk@techfivesystems.com>
2016-08-19 11:00:15 +0530

    Commented youtube local  redirect uri

--- application/config/youtube.php
+++ application/config/youtube.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['youtube_client_id'] = '495277179424-i6ekqt1hqur10r1iq2th91tl80es69at.apps.googleusercontent.com';
 $config['youtube_client_secret'] = 'Osp3L21dglv4XKya5kxeHQsQ';
-$config['redirect_uri'] = "http://local.timeframe.com/Youtube_connect/youtube_callback";
+// $config['redirect_uri'] = "http://local.timeframe.com/Youtube_connect/youtube_callback";
 
 /*
 	Server App setting
